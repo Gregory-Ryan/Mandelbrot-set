04a99fffa8c1fddafba011b5692318da548db9b1
Gregory-Ryan <44442147+Gregory-Ryan@users.noreply.github.com>
2018-10-30 12:17:32 -0400

    Update cf.py

--- cf.py
+++ cf.py
@@ -437,22 +437,3 @@ def F(z,n) :
         if mod(subt(z4,multi(0.00000000999,z4))) <= mod(z1) <= mod(add(z4,multi(0.00000000999,z4)))   :
             return z4 
     return math.inf
-
-def H_n(n) :
-    #Calculates the Hermonic Series the nth term
-    z1 = 0
-    for i in range(1, n + 1) :
-        z2 = 1 / i
-        z1 = z1 + z2
-    return z1
-    
-def polygamma(n) :
-    #Calculates the polygamma function (Only real int)
-    z1 = 1 - math.log(2)
-    for i in range(2,1000000) :
-        z2 = 1 / i - math.log(1 + (1 / i))
-        z1 = z1 + z2
-    z3 = H_n(n - 1) - z1
-    return z3
-    
-euler_c = - polygamma(1)
