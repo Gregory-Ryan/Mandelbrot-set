e82857cb0af229d09b2490a206991bb8e879bf3d
Gregory-Ryan <44442147+Gregory-Ryan@users.noreply.github.com>
2018-10-30 12:01:52 -0400

    Update cf.py

--- cf.py
+++ cf.py
@@ -440,15 +440,15 @@ def F(z,n) :
 
 def H_n(n) :
     #Calculates the Hermonic Series the nth term
-    z1 = 1
-    for i in range(2, n + 1) :
+    z1 = 0
+    for i in range(1, n + 1) :
         z2 = 1 / i
         z1 = z1 + z2
     return z1
     
 def polygamma(n) :
     #Calculates the polygamma function (Only real int)
-    z1 = 1
+    z1 = 1 - math.log(2)
     for i in range(2,1000000) :
         z2 = 1 / i - math.log(1 + (1 / i))
         z1 = z1 + z2
